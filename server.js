2f1bed9b1f1751f9f594b6f03a38c3532249f5ec
Osaze Oviawe <ocubed88@gmail.com>
2019-07-13 15:13:55 +0100

    Express server set up

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) =>
+  res
+    .status(200)
+    .json({ status: 'Success', message: 'Hello from the server side!!!' })
+);
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/auth', require('./routes/auth'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
